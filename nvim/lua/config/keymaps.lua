6e423b254f99677f5ebab56576c996f8f2c7ba5a
Denis Andrei Szabo <denisandrei.szabo@gmail.com>
2025-10-29 11:59:58 +0100

    Updated nvim keymaps
    Updated delete to not yank on delete, delete line to not yank on delete
    line and change to not yank on change.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete without yanking
+vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "x" }, "D", '"_D', { desc = "Delete line without yanking" })
+vim.keymap.set({ "n", "x" }, "c", '"_c', { desc = "Change without yanking" })

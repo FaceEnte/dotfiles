0e3b8ae8bc99b3fa6dd3dfb2cc467a524ce4d7a8
Denis Andrei Szabo <denisandrei.szabo@gmail.com>
2025-10-30 12:14:23 +0100

    Updated nvim keymaps
    Replacing text with pasting over selection doesnt yank the replaced text
    anymore

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@
 vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete without yanking" })
 vim.keymap.set({ "n", "x" }, "D", '"_D', { desc = "Delete line without yanking" })
 vim.keymap.set({ "n", "x" }, "c", '"_c', { desc = "Change without yanking" })
+
+-- Paste without yanking
+vim.keymap.set("x", "p", '"_dP', { desc = "Paste without yanking replaced text" })
